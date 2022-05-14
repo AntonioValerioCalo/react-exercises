c558c9108f3ea3587ba9c51a4a226d6982cf2798
AntonioValerioCalo <76622946+AntonioValerioCalo@users.noreply.github.com>
2022-05-14 20:20:28 +0200

    props-05

--- react-exercise/src/Message.js
+++ react-exercise/src/Message.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export class Message extends React.Component{
     render(){
-        return <p>What a beatiful day</p>
+        return <p>What a beatiful day </p>
     }
 }
