19d03a1517bf4ebe34ddd21a0b476ea4b811718e
AntonioValerioCalo <76622946+AntonioValerioCalo@users.noreply.github.com>
2022-05-15 14:59:58 +0200

    conditional-rendering-01

--- react-exercise/src/Age.js
+++ react-exercise/src/Age.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-export class Age extends React.Component{
-    render(){
-        return <p>Your age is {this.props.age}</p>
-    }
+export class Age extends React.Component {
+  render() {
+    return <div>{this.props.age>18 && <p>Your age is {this.props.age}</p>}</div>;
+  }
 }
