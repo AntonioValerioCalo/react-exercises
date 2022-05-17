5075daf9c082a5d3217bef86e43c3748db86eb9f
AntonioValerioCalo <76622946+AntonioValerioCalo@users.noreply.github.com>
2022-05-17 17:22:54 +0200

    events-03

---
+++ react-exercise/src/ClickTrucker.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export class ClickTrucker extends React.Component {
+  state = {
+    trucker: "",
+  };
+
+  eventHandler = (event) => {
+    this.setState({
+      trucker: event.target.name,
+    });
+  };
+  render(){
+      return(
+          <div>
+              <h1> trucker:{this.state.trucker }</h1>
+              <button onClick={this.eventHandler} name="first">first</button>
+              <button onClick={this.eventHandler} name="second">second</button>
+              <button onClick={this.eventHandler} name="third">third</button>
+          </div>
+      )
+
+      
+  }
+}
