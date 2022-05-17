1f666c421a1a62c363759e659e0982dde765a2c7
AntonioValerioCalo <76622946+AntonioValerioCalo@users.noreply.github.com>
2022-05-17 16:42:36 +0200

    events-01

---
+++ react-exercise/src/ClickCounter.js
@@ -0,0 +1,23 @@
+import React from "react";  
+
+
+export class ClickCounter extends React.Component{
+    state ={
+        count:0,
+    }
+    handleClick=()=>{
+        this.setState(({count})=>({
+          count: count +1
+        }))
+    }
+    render(){
+        return (
+            <div>
+            <h3>{this.state.count}</h3>
+            <button onClick={this.handleClick}>increment</button>
+            </div>
+        )
+
+        
+    }
+}
