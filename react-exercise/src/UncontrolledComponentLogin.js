a6fb4ecc50d3f09d0a989f17f9344c0f157a1501
AntonioValerioCalo <76622946+AntonioValerioCalo@users.noreply.github.com>
2022-05-19 18:06:53 +0200

    forms-06

--- react-exercise/src/UncontrolledComponentLogin.js
+++ react-exercise/src/UncontrolledComponentLogin.js
@@ -1,41 +1,36 @@
-import React, {createRef} from "react";  
+import React, { createRef } from "react";
 
-export class UncontrolledLogin extends React.Component{
-    _formRef = createRef();
-    handleForm=(event)=>{
-        event.preventDefault()
-        const username= event.target.elements.username.value
-         const password =event.target.elements.password.value
-         const remember =event.target.elements.remember.cheked 
-         console.log({
-             username,
-             password,
-             remember,
-         })
-      }
-  
-    render(){
-        return (
-            <div>
-          <h1>Uncontrolled Components</h1>
-          <form ref={this._formRef} onSubmit={this.handleForm}>
-              <input 
-              type="text"
-              name="username"
-              />
-              <input
-              type="password"
-              name="password"
-              />
-              <input
-              type="checkbox"
-              name="remember"
-             /> 
-            <button type="submit" disabled={this.disabledButton} >login</button>
-            <button type="reset">reset</button>
-          </form>
+export class UncontrolledLogin extends React.Component {
+  _formRef = createRef();
+  handleForm = (event) => {
+    event.preventDefault();
+    const username = event.target.elements.username.value;
+    const password = event.target.elements.password.value;
+    const remember = event.target.elements.remember.cheked;
+    console.log({
+      username,
+      password,
+      remember,
+    });
+  };
+   componentDidMount=(event)=>{
+       this._formRef.current.focus()
+   }
 
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1>Uncontrolled Components</h1>
+        <form  onSubmit={this.handleForm}>
+          <input type="text" ref={this._formRef} name="username" />
+          <input type="password" name="password" />
+          <input type="checkbox" name="remember" />
+          <button type="submit" disabled={this.disabledButton}>
+            login
+          </button>
+          <button type="reset">reset</button>
+        </form>
+      </div>
+    );
+  }
 }
