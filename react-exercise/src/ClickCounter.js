76d0cfc18269f1b4fc04db4a5933756d68a8b75c
AntonioValerioCalo <76622946+AntonioValerioCalo@users.noreply.github.com>
2022-05-25 19:52:51 +0200

    useState-01

--- react-exercise/src/ClickCounter.js
+++ react-exercise/src/ClickCounter.js
@@ -1,22 +1,43 @@
 
-import React from "react";
+import { useState } from "react";
+export function ClickCounter({initialValue=0}){
+  const [counter, setCounter] = useState(initialValue)
+    function handleIncrement(){
+      setCounter(c => c + 1)
+    }
+  return(
+    <div>
+      <h2>{counter}</h2>
+      <button type="button" onClick={handleIncrement}>increment</button>
+    </div>
+  )
 
-export class ClickCounter extends React.Component {
-  state = {
-    count: 0,
-  };
-  handleClick = () => {
-    this.setState(({ count }) => ({
-      count: count + 1,
-    }));
-  };
-  render() {
-    return (
-      <div>
-        <h3>{this.state.count}</h3>
-        <button onClick={this.handleClick}>increment</button>
-      </div>
-    );
-  }
 }
+
+
+
+
+
+
+
+// import React from "react";
+
+// export class ClickCounter extends React.Component {
+//   state = {
+//     count: 0,
+//   };
+//   handleClick = () => {
+//     this.setState(({ count }) => ({
+//       count: count + 1,
+//     }));
+//   };
+//   render() {
+//     return (
+//       <div>
+//         <h3>{this.state.count}</h3>
+//         <button onClick={this.handleClick}>increment</button>
+//       </div>
+//     );
+//   }
+// }
 
