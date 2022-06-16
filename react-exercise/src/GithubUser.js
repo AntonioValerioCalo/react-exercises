1d0e3a9da6d1ba5a13461b33050938bc3845ac86
AntonioValerioCalo <76622946+AntonioValerioCalo@users.noreply.github.com>
2022-06-16 14:44:43 +0200

    redux-01

--- react-exercise/src/GithubUser.js
+++ react-exercise/src/GithubUser.js
@@ -1,7 +1,10 @@
 
 import { UseGithubuser } from "./UseGithubuser";
-export function GithubUser(){
-const {data,error,oneRefresh}=UseGithubuser()
+export function GithubUser({username}){
+const {data,error,oneRefresh}=UseGithubuser(username)
+function handleFetch(){
+    oneRefresh(username)
+}
     // const {data,load,error} = UseGithubuser(username);
 // const [data , setData] = useState(null);
 
@@ -19,14 +22,14 @@ const {data,error,oneRefresh}=UseGithubuser()
 // },[username]) 
     return(
         <div>
-         <button onClick={oneRefresh}>Refresh</button>
+         <button onClick={handleFetch}>Refresh</button>
          {!data && !error && <h1>...loading</h1>}
          {error && <h1>there has been error</h1>}
-        {data && !error && data.map((user)=>(<ul>
+         {data && !error && <h1>{data.login}</h1>}{/* data.map((user)=>(<ul>
             <li >
                 {user.login}
             </li>
-        </ul>))}
+        </ul>))} */}
     </div>
     )
 }
