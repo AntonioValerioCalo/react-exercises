c53968b4b4d06bcf538facaaf99ae29cc1155616
AntonioValerioCalo <76622946+AntonioValerioCalo@users.noreply.github.com>
2022-06-18 18:33:46 +0200

    redux-03

--- react-exercise/src/state/CounterReducer.js
+++ react-exercise/src/state/CounterReducer.js
@@ -1,43 +1,55 @@
-const defaulState=0;
+import { createSlice } from "@reduxjs/toolkit";
 
-const INCREMENT="COUNTER@INCREMENT"
-const DECREMENT="COUNTER@DECREMENT"
-const RESET="COUNTER@RESET"
 
-export function inncrementCounter(by=1){
-return{
-    type:INCREMENT,
-    payload:by,
-}
-}
+ export const counterState=createSlice({
+  name: "counter",
+  initialState: 0,
+  reducers: {
+    increment: (state, action) => state + action.payload,
+    decrement: (state, action) => state - action.payload,
+    reset: (state, action)=> 0,
+  },
+});
 
-export function decrementCounter(by=1){
-  return{  type:DECREMENT,
-    payload:by,
-}
-}
+// const defaulState=0;
 
-export function resetCounter(){
-    return{
-        type:RESET,
-    }
-}
+// const INCREMENT="COUNTER@INCREMENT"
+// const DECREMENT="COUNTER@DECREMENT"
+// const RESET="COUNTER@RESET"
 
-export function counterReducer(state=defaulState,action){
-    switch(action.type){
-        case INCREMENT:{
-         return state + action.payload
-        }
-        case DECREMENT:{
-            return state - action.payload
-        }
-        case RESET:{
-            return defaulState
-        } 
-        default:{
-            return state 
-        }
-        
-     
-    }
-}
+// export function inncrementCounter(by=1){
+// return{
+//     type:INCREMENT,
+//     payload:by,
+// }
+// }
+
+// export function decrementCounter(by=1){
+//   return{  type:DECREMENT,
+//     payload:by,
+// }
+// }
+
+// export function resetCounter(){
+//     return{
+//         type:RESET,
+//     }
+// }
+
+// export function counterReducer(state=defaulState,action){
+//     switch(action.type){
+//         case INCREMENT:{
+//          return state + action.payload
+//         }
+//         case DECREMENT:{
+//             return state - action.payload
+//         }
+//         case RESET:{
+//             return defaulState
+//         }
+//         default:{
+//             return state
+//         }
+
+//     }
+// }
--- react-exercise/src/state/Store.js
+++ react-exercise/src/state/Store.js
@@ -1,13 +1,14 @@
 import {createStore,combineReducers} from 'redux'
-import{ counterReducer,inncrementCounter,decrementCounter,resetCounter} from "./CounterReducer"
+import { counterState } from './CounterReducer'
+
 import { userReducer } from './TodosReducer'
  
  const rootReducer=combineReducers({
-    counter:counterReducer,
+    counter:counterState,
     user:userReducer,
 })
  export const store = createStore(rootReducer)
  
- store.dispatch(inncrementCounter(1))
- store.dispatch(decrementCounter(6))
- store.dispatch(resetCounter(0))
+//  store.dispatch(inncrementCounter(1))
+//  store.dispatch(decrementCounter(6))
+//  store.dispatch(resetCounter(0))
